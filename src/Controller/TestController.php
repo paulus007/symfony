6586732c7c322e9b99f9467d9c0c6775c9e84c12
spman <spman.ru@gmail.com>
2025-10-20 14:33:46 +0300

    # dev

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,16 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TestController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $param = $request->query->getString('param');
+
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
+            'param'           => $param,
         ]);
     }
 }

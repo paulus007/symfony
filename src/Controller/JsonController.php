17fb959257acd290aa0ad422665dbab354f1db07
spman <spman.ru@gmail.com>
2025-11-09 23:46:34 +0300

    # dev

--- src/Controller/JsonController.php
+++ src/Controller/JsonController.php
@@ -4,15 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class JsonController extends AbstractController
 {
     #[Route('/json', name: 'app_json')]
-    public function index(): JsonResponse
+    public function jsonApp(): JsonResponse
     {
         return new JsonResponse([
             'success' => true,
         ]);
+    }
+
+    #[Route('/text/{pos}', name: 'app_text')]
+    public function textApp(string $pos = 'undefined'): Response
+    {
+        return new Response("<b>{$pos}</b>");
     }
 }
